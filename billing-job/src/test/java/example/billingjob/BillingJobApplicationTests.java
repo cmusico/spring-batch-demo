78fa95db2be1cf6e780b780951ef02511c61680f
cmusico <149638306+cmusico@users.noreply.github.com>
2024-01-25 14:35:09 -0800

    Used ItemProcessor
    Processed data using ItemProcessor and used it in chunk-oriented step

--- billing-job/src/test/java/example/billingjob/BillingJobApplicationTests.java
+++ billing-job/src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -16,6 +16,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @SpringBatchTest
@@ -23,19 +24,19 @@ import java.nio.file.Paths;
 class BillingJobApplicationTests {
 
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-	@Autowired
 	private JobLauncherTestUtils jobLauncherTestUtils;
 
 	@Autowired
 	private JobRepositoryTestUtils jobRepositoryTestUtils;
-    
-    @BeforeEach
-    public void setUp() {
-        this.jobRepositoryTestUtils.removeJobExecutions();
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@BeforeEach
+	public void setUp() {
+		this.jobRepositoryTestUtils.removeJobExecutions();
 		JdbcTestUtils.deleteFromTables(this.jdbcTemplate, "BILLING_DATA");
-    }
+	}
 
 	@Test
 	void testJobExecution() throws Exception {
@@ -51,6 +52,10 @@ class BillingJobApplicationTests {
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 		Assertions.assertTrue(Files.exists(Paths.get("staging", "billing-2023-01.csv")));
 		Assertions.assertEquals(1000, JdbcTestUtils.countRowsInTable(jdbcTemplate, "BILLING_DATA"));
+
+		Path billingReport = Paths.get("staging", "billing-report-2023-01.csv");
+		Assertions.assertTrue(Files.exists(billingReport));
+		Assertions.assertEquals(781, Files.lines(billingReport).count());
 	}
 
 }
